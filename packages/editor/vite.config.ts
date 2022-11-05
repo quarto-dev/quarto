3232c29b747271a741dd8127d64c6b5966bc54ed
J.J. Allaire <jj.allaire@gmail.com>
2022-11-05 17:12:37 -0400

    externalize lib

---
+++ packages/editor/vite.config.ts
@@ -0,0 +1,20 @@
+// vite.config.ts
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  plugins: [dts()],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'editor',
+      fileName: 'editor'
+    },
+    rollupOptions: {
+      
+    },
+    emptyOutDir: false,
+  }
+})

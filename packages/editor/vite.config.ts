b00087f6fc7086bcd8dbc6f552e1bec9764836d0
J.J. Allaire <jj.allaire@gmail.com>
2022-11-06 08:14:36 -0500

    allow environment to drive configuration

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -1,18 +1,30 @@
 // vite.config.ts
 import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, LibraryFormats } from 'vite'
 import dts from 'vite-plugin-dts'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+
+// allow environment to drive configuration
+const types = !(process.env.PANMIRROR_TYPES === "0");
+const formats = (process.env.PANMIRROR_FORMATS || "es").split(",") as LibraryFormats[];
+const outDir = process.env.PANMIRROR_OUT_DIR || "dist";
+
+// setup plugins
+const plugins = [cssInjectedByJsPlugin()];
+if (types) {
+  plugins.push(dts());
+}
 
 export default defineConfig({
   define: {
     'process.env.NODE_ENV': '"production"'
   },
-  plugins: [dts(), cssInjectedByJsPlugin()],
+  plugins,
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'Panmirror',
+      formats,
       fileName: (format) => `panmirror.${format === 'umd' ? 'js' : format === 'es' ? 'mjs' : format + '.js'}` 
     },
     rollupOptions: {
@@ -21,6 +33,7 @@ export default defineConfig({
       },
     },
     sourcemap: true,
+    outDir,
     emptyOutDir: false,
   }
 })

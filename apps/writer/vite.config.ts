d3efd5b904099702621b9613b78a3e62bf68cd56
J.J. Allaire <jj.allaire@gmail.com>
2022-11-07 05:58:17 -0500

    proxy to writer-server

---
+++ apps/writer/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5001",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});

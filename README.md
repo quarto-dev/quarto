55140322bbb6b4dca829336ccc9b631e21912b61
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2022-11-09 10:53:09 -0700

    Squashed 'packages/quarto-ojs-runtime/' content from commit 60fe9f9
    git-subtree-dir: packages/quarto-ojs-runtime
    git-subtree-split: 60fe9f90c67dcba5a93a810dee35c224113fc29d

---
+++ README.md
@@ -0,0 +1,4 @@
+# @quarto/quarto-ojs-runtime
+
+A runtime for [Quarto](https://quarto.org)'s support of [ObservableHQ's reactive Javascript](https://observablehq.com).
+
---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from "rollup-plugin-terser";
+import * as meta from "./package.json";
+
+const config = {
+  input: "src/index.js",
+  external: Object.keys(meta.dependencies || {}).filter(key => /^d3-/.test(key)),
+  output: {
+    file: "dist/quarto-ojs-runtime.js",
+    name: "quarto",
+    format: "esm",
+    indent: false,
+    extend: true,
+    banner: `// ${meta.name} v${meta.version} Copyright ${(new Date).getFullYear()} ${meta.author.name}`,
+    globals: ["$", "Shiny"]
+  },
+  plugins: [commonjs(), nodeResolve({
+    mainFields: ["module", "main"],
+  })]
+};
+
+export default [
+  config,
+  {
+    ...config,
+    output: {
+      ...config.output,
+      file: "dist/quarto-ojs-runtime.min.js",
+      name: "quarto-min",
+    },
+    plugins: [
+      ...config.plugins,
+      terser({
+        mangle: {
+          reserved: ["RequireError"]
+        }
+      }),
+    ]
+  }
+];

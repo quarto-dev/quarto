4b7698df5ac54e88efe6f3893a2e75dcdad5f90e
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2022-11-10 09:54:44 -0700

    remove quarto-core-lib

--- packages/quarto-core-lib/src/yaml-intelligence/README.md
+++
@@ -1,3 +0,0 @@
-## yaml-intelligence
-
-Code in this module is meant to be used in the IDE.
--- packages/quarto-core-lib/src/yaml-intelligence/vs-code.ts
+++
@@ -1,8 +0,0 @@
-/*
-* web-worker.ts
-*
-* Copyright (C) 2022 by RStudio, PBC
-*
-*/
-
-export { getCompletions, getHover, getLint } from "./yaml-intelligence.js";

5b119fb511528d4d808d992382a0cbf6317b6fa8
J.J. Allaire <jj.allaire@gmail.com>
2022-11-10 16:30:30 -0500

    add react-i81next

--- apps/writer/src/i18next.ts
+++ apps/writer/src/i18next.ts
@@ -16,11 +16,13 @@
 import i18n, { TFunction } from 'i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
 
 export async function i18nInit(): Promise<TFunction> {
   t = await i18n
     .use(Backend)
     .use(LanguageDetector)
+    .use(initReactI18next)
     .init({
       fallbackLng: 'en',
       debug: false,

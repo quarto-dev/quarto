5fbf84f72ffea78bff959b2c1268d00f73942463
J.J. Allaire <jj.allaire@gmail.com>
2022-11-14 13:22:57 -0500

    migrate to popover2

--- apps/writer/src/index.tsx
+++ apps/writer/src/index.tsx
@@ -25,6 +25,8 @@ import Workbench from './workbench/Workbench';
 import 'normalize.css/normalize.css';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+import "@blueprintjs/popover2/lib/css/blueprint-popover2.css";
+import "@blueprintjs/select/lib/css/blueprint-select.css";
 import { i18nInit } from './i18n';
 
 async function runApp() {

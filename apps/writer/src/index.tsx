4be6227175eb017f8137ac2d8ee8f02cb8e42be7
J.J. Allaire <jj.allaire@gmail.com>
2022-11-15 14:23:49 -0500

    less menu padding

--- apps/writer/src/index.tsx
+++ apps/writer/src/index.tsx
@@ -27,6 +27,7 @@ import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 import "@blueprintjs/popover2/lib/css/blueprint-popover2.css";
 import "@blueprintjs/select/lib/css/blueprint-select.css";
+import "./styles.scss"
 import { i18nInit } from './i18n';
 
 async function runApp() {

efd09dd04f6dd7fab11dcf04d13e09d82e62da42
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2022-11-16 09:22:50 -0500

    Squashed 'packages/ojs/external-d3-d3-require/' content from commit d8c0b4c
    git-subtree-dir: packages/ojs/external-d3-d3-require
    git-subtree-split: d8c0b4c476d3ef46046e20fe9912769e5f7e75ef

---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+import {terser} from "rollup-plugin-terser";
+import * as meta from "./package.json";
+
+const config = {
+  input: "src/index.mjs",
+  external: Object.keys(meta.dependencies || {}).filter(key => /^d3-/.test(key)),
+  output: {
+    file: `dist/${meta.name}.js`,
+    name: "d3",
+    format: "umd",
+    indent: false,
+    extend: true,
+    banner: `// ${meta.name} v${meta.version} Copyright ${(new Date).getFullYear()} ${meta.author.name}`,
+    globals: Object.assign({}, ...Object.keys(meta.dependencies || {}).filter(key => /^d3-/.test(key)).map(key => ({[key]: "d3"})))
+  },
+  plugins: []
+};
+
+export default [
+  config,
+  {
+    ...config,
+    output: {
+      ...config.output,
+      file: `dist/${meta.name}.min.js`
+    },
+    plugins: [
+      ...config.plugins,
+      terser({
+        output: {
+          preamble: config.output.banner
+        },
+        mangle: {
+          reserved: ["RequireError"]
+        }
+      })
+    ]
+  }
+];

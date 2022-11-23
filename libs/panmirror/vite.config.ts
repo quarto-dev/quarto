9e1de2c70b5980fa16c040ce2983953df84d554a
J.J. Allaire <jj.allaire@gmail.com>
2022-11-23 11:28:06 -0500

    build panmirror in its own lib dir

---
+++ libs/panmirror/vite.config.ts
@@ -0,0 +1,34 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+
+// allow environment to drive output dir
+const outDir = process.env.PANMIRROR_OUTDIR || "dist";
+
+// setup plugins
+const plugins = [cssInjectedByJsPlugin()];
+
+export default defineConfig({
+  define: {
+    'process.env.NODE_ENV': '"production"',
+    'process.env.TERM': '""',
+    'process.platform': '""'
+  },
+  plugins,
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'Panmirror',
+      formats: ['umd'],
+      fileName: () => 'panmirror.js' 
+    },
+    rollupOptions: {
+      output: {
+        assetFileNames: "panmirror.[ext]",
+      },
+    },
+    sourcemap: false,
+    outDir,
+    emptyOutDir: false,
+  }
+})

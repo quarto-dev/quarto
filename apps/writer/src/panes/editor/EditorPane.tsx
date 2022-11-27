14fcb0de296cd6ced9889f5f0556bbadb361b93d
J.J. Allaire <jj.allaire@gmail.com>
2022-11-27 08:20:25 -0500

    use react.fc for editor pane

--- apps/writer/src/panes/editor/EditorPane.tsx
+++ apps/writer/src/panes/editor/EditorPane.tsx
@@ -13,200 +13,257 @@
  *
  */
 
-import React from 'react';
+import React, { useCallback, useContext, useEffect, useRef } from 'react';
 
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { useTranslation } from 'react-i18next';
 
 import { Intent, Spinner } from '@blueprintjs/core';
 
 import { 
   Editor, 
-  EditorDialogs, 
-  EditorOutline, EventType, 
+  EventType, 
   kAlertTypeError, 
   NavigationType,
   UpdateEvent, 
   OutlineChangeEvent, 
   StateChangeEvent,
   EditorFormat,
-  kQuartoDocType
+  kQuartoDocType,
+  EditorDialogs
 } from 'editor';
 
-import { t } from '../../i18n';
-
-import { CommandManager, withCommandManager } from '../../commands/CommandManager';
-import { WorkbenchState } from '../../store/store';
 import {
+  editorMarkdown,
+  editorTitle,
+  editorLoading,
   setEditorMarkdown,
   setEditorSelection,
   setEditorOutline,
   setEditorTitle,
   setEditorLoading,
 } from '../../store/editor';
+import { prefsShowMarkdown } from '../../store/prefs';
+
+import { CommandManager, CommandManagerContext } from '../../commands/CommandManager';
+
 import { Pane } from '../../widgets/Pane';
 
-
-import { editorProsemirrorCommands, editorExternalCommands, editorDebugCommands } from './editor-commands';
-import { EditorActionsContext } from './EditorActionsContext';
-import EditorToolbar from './EditorToolbar';
-
+import { editorDialogs } from './dialogs/editor-dialogs';
 import EditorDialogsImpl from './dialogs/EditorDialogsImpl';
 import EditorOutlineSidebar from './outline/EditorOutlineSidebar';
-
 import { editorContext } from './context/editor-context';
-import { editorDialogs } from './dialogs/editor-dialogs';
+
+import { editorProsemirrorCommands, editorExternalCommands, editorDebugCommands } from './editor-commands';
+import { EditorActions, EditorActionsContext } from './EditorActionsContext';
+import EditorToolbar from './EditorToolbar';
+
+import { PandocFormat } from 'editor/src/api/pandoc_format';
 
 import styles from './EditorPane.module.scss';
 
+const EditorPane : React.FC = () => {
 
-interface EditorPaneProps {
-  loading: boolean;
-  title: string;
-  markdown: string;
-  showMarkdown: boolean;
-  setLoading: (loading: boolean) => void;
-  setTitle: (title: string) => void;
-  setMarkdown: (markdown: string) => void;
-  setOutline: (outline: EditorOutline) => void;
-  setSelection: (selection: unknown) => void;
-  commandManager: CommandManager;
-}
+  // global services
+  const { t } = useTranslation();
+  const commandManager = useContext(CommandManagerContext);
 
-class EditorPane extends React.Component<EditorPaneProps> {
-  // container for the editor
-  private parent: HTMLDivElement | null;
+  // redux state
+  const title = useSelector(editorTitle);
+  const loading = useSelector(editorLoading);
+  const markdown = useSelector(editorMarkdown);
+  const showMarkdown = useSelector(prefsShowMarkdown);
+  const dispatch = useDispatch();
 
-  // editor instance
-  private editor: Editor | null;
+  // refs we get from rendering
+  const parentRef = useRef<HTMLDivElement>(null);
+  const editorDialogsRef = useRef<EditorDialogsImpl>(null);
 
-  // events we need to unsubscibe from when we are unmounted
-  private editorEvents: VoidFunction[];
-
-  // we track the markdown last sent to / observed within the editor
-  // so that we can mask out updates that don't change the content
-  // (this is neccessary to prevent update loops)
-  private editorMarkdown: string | null;
-
-  // services that we provide to the core editor
-  private editorDialogsRef: React.RefObject<EditorDialogsImpl>;
-
-  constructor(props: Readonly<EditorPaneProps>) {
-    super(props);
-    this.state = { loading: true };
-    this.parent = null;
-    this.editor = null;
-    this.editorEvents = [];
-    this.editorMarkdown = null;
-    this.editorDialogsRef = React.createRef<EditorDialogsImpl>();
-    this.onResize = this.onResize.bind(this);
+  // refs that hold out of band state 
+  const editorRef = useRef<Editor | null>(null);
+  const showMarkdownRef = useRef<boolean | null>(null);
+ 
+  // subscribe/unsubscribe from editor events
+  const editorEventsRef = useRef(new Array<VoidFunction>());
+  function onEditorEvent<T>(event: EventType<T>, handler: (detail?: T) => void) {
+    editorEventsRef.current.push(editorRef.current!.subscribe(event, handler));
+  }
+  const unregisterEditorEvents = () => {
+    editorEventsRef.current.forEach(unregister => unregister());
   }
 
-  public render() {
-    return (
-      <Pane className={'editor-pane'}>
-        <EditorActionsContext.Provider value={this}>
-          <EditorToolbar />
-          <div id="editor" className={styles.editorParent} ref={el => (this.parent = el)}>
-            {this.editorLoading()}
-            <EditorOutlineSidebar />
-          </div>
-          <EditorDialogsImpl ref={this.editorDialogsRef} />
-        </EditorActionsContext.Provider>
-      </Pane>
-    );
+  // general helper functions
+  const errorAlert = (error: unknown) => {
+    const message = error instanceof Error ? error.message : String(error);
+    editorDialogsRef.current?.alert(message, t('error_alert_title') as string, kAlertTypeError);
   }
 
-  public async componentDidMount() {
+  // initialize the editor
+  const initEditor = useCallback(async () => {
+    
+    editorRef.current = await createEditor(parentRef.current!, () => commandManager, editorDialogs(editorDialogsRef.current!));
+    
+    window.addEventListener("resize", onResize);
 
-    const editor = await this.createEditor();
-    if (!editor) {
-      return;
-    }
-    this.editor = editor;
-        
-    window.addEventListener("resize", this.onResize);
-
-    // show any warnings
-    this.showPandocWarnings();
+    showPandocWarnings(editorRef.current?.getPandocFormat());
 
     // subscribe to events
-    this.onEditorEvent(UpdateEvent, this.onEditorDocChanged);
-    this.onEditorEvent(OutlineChangeEvent, this.onEditorOutlineChanged);
-    this.onEditorEvent(StateChangeEvent, this.onEditorStateChanged);
+    onEditorEvent(UpdateEvent, onEditorDocChanged);
+    onEditorEvent(OutlineChangeEvent, onEditorOutlineChanged);
+    onEditorEvent(StateChangeEvent, onEditorStateChanged);
 
     // add commands
-    this.props.commandManager.addCommands([
-      ...editorProsemirrorCommands(this.editor!.commands()),
-      ...editorExternalCommands(this.editor!),
-      ...editorDebugCommands(this.editor!),
+    commandManager.addCommands([
+      ...editorProsemirrorCommands(editorRef.current!.commands()),
+      ...editorExternalCommands(editorRef.current!),
+      ...editorDebugCommands(editorRef.current!),
     ]);
 
     // set menus
-    this.props.commandManager.setMenus(this.editor!.getMenus());
+    commandManager.setMenus(editorRef.current!.getMenus());
 
-    // update editor
-    await this.updateEditor();
-
-    // sync title
-    this.syncEditorTitle();
-  }
-
-  private onResize() {
-    this.editor!.resize();
-  }
-
-  public componentWillUnmount() {
-    this.editorEvents.forEach(unregister => unregister());
-    window.removeEventListener("resize", this.onResize);
-  }
-
-  public componentDidUpdate(prevProps: EditorPaneProps) {
-    // ignore if the editor is not yet loaded
-    if (!this.editor) {
-      return;
+    // load editor
+    await editorRef.current!.setMarkdown(markdown, panmirrorWriterOptions(), false);
+    dispatch(setEditorTitle(editorRef.current?.getTitle() || ''));
+    onEditorOutlineChanged();
+    if (loading) {
+      dispatch(setEditorLoading(false));
+      editorRef.current?.focus();
     }
+     
+  }, []);
 
-    // if showMarkdown changed to true then save markdown
-    if (this.props.showMarkdown && !prevProps.showMarkdown) {
-      this.saveMarkdown();
-    }
-
-    // update editor
-    this.updateEditor();
-  }
-
-  // implement EditorActions interface by proxing to this.editor --
-  // we need to do this rather than just passing the this.editor! as
-  // the value b/c we wait until after rendering to actually create
-  // the editor (because it needs a live DOM element as it's parent)
-  public focus() {
-    if (this.editor) {
-      this.editor.focus();
-    }
-  }
-  public navigate(id: string) {
-    if (this.editor) {
-      this.editor.navigate(NavigationType.Id, id);
+  // provide EditorActionsContext from editor 
+  const editorActions: EditorActions = {
+    focus: function (): void {
+      editorRef.current?.focus();
+    },
+    navigate: function (id: string): void {
+      editorRef.current?.navigate(NavigationType.Id, id);
     }
   }
 
-  private editorLoading() {
-    if (this.props.loading) {
-      return this.props.loading && (
-        <div className={['ProseMirror', styles.editorLoading].join(' ')}>
-          <div className='body pm-editing-root-node pm-text-color pm-background-color'>
-            <Spinner className={styles.editorLoadingSpinner} intent={Intent.NONE} ></Spinner>
-          </div>
+  // when doc changes, propagate markdown only if showMarkdown is true (as
+  // (this is an incredibly expensive operation to run on every keystroke!)
+  const onEditorDocChanged = () => {
+    if (showMarkdownRef.current) {
+      saveMarkdown();
+    }
+    dispatch(setEditorTitle(editorRef.current?.getTitle() || ''));
+  };
+
+  // dispatch outline changes
+  const onEditorOutlineChanged = () => {
+     const outline = editorRef.current?.getOutline();
+     if (outline) {
+       dispatch(setEditorOutline(outline));
+     }
+  }
+
+  // dispatch selection changes (allows command manager to update)
+  const onEditorStateChanged = () => {
+    dispatch(setEditorSelection(editorRef.current!.getSelection()));
+  }
+
+  // save editor markdown to the redux store
+  const saveMarkdown = async () => {
+    try {
+      // generate markdown
+      const markdown = await editorRef.current!.getMarkdown(panmirrorWriterOptions());
+     
+      // set markdown into redux
+      dispatch(setEditorMarkdown(markdown.code));
+     
+    } catch (error) {
+      errorAlert(error);
+    }
+  }
+
+  // propagate window resize to editor
+  const onResize = () => {
+    editorRef.current?.resize();
+  }
+
+  // editor initialization
+  useEffect(() => {
+    initEditor().catch(error => {
+      errorAlert(error);
+    });
+
+    return () => {
+      unregisterEditorEvents();
+      window.removeEventListener("resize", onResize);
+    }
+  }, []);
+
+  // update editor title when it changes
+  useEffect(() => {
+    if (title !== editorRef.current?.getTitle()) {
+      editorRef.current?.setTitle(title);
+    }
+  }, [title]);
+
+  // update markdown in store when pref changes (also
+  // note value for out-of-band editor changed callback)
+  useEffect(() => {
+    showMarkdownRef.current = showMarkdown;
+    if (editorRef.current && showMarkdown) {
+      saveMarkdown();
+    }
+  }, [showMarkdown]);
+
+  // render
+  return (
+    <Pane className={'editor-pane'}>
+      <EditorActionsContext.Provider value={editorActions}>
+        <EditorToolbar />
+        <div id="editor" className={styles.editorParent} ref={parentRef}>
+          {editorLoadingUI(loading)}
+          <EditorOutlineSidebar />
         </div>
-      )
-    } else {
-      return <div/>;
-    }
-  }
+        <EditorDialogsImpl ref={editorDialogsRef} />
+      </EditorActionsContext.Provider>
+    </Pane>
+  );
+}
 
-  
-  private async createEditor() : Promise<Editor | undefined> {
-    const context = editorContext(() => this.props.commandManager, this.editorDialogs);
+const panmirrorWriterOptions = () => {
+  return {
+    atxHeaders: true
+  };
+}
+
+const showPandocWarnings = (pandocFormat?: PandocFormat) => {
+  const warnings = pandocFormat?.warnings;
+  if (warnings?.invalidFormat) {
+    // console.log('WARNING: invalid pandoc format ' + warnings.invalidFormat);
+  }
+  if (warnings?.invalidOptions.length) {
+    // console.log(`WARNING: ${pandocFormat.baseName} does not support options: ${warnings.invalidOptions.join(', ')}`);
+  }
+}
+
+const editorLoadingUI = (loading: boolean) => {
+  if (loading) {
+    return loading && (
+      <div className={['ProseMirror', styles.editorLoading].join(' ')}>
+        <div className='body pm-editing-root-node pm-text-color pm-background-color'>
+          <Spinner className={styles.editorLoadingSpinner} intent={Intent.NONE} ></Spinner>
+        </div>
+      </div>
+    )
+  } else {
+    return <div/>;
+  }
+}
+
+const createEditor = async (
+  parent: HTMLElement, 
+  commandManager: () => CommandManager, 
+  dialogs: EditorDialogs
+) : Promise<Editor> => {
+  const context = editorContext(commandManager, dialogs);
     const format: EditorFormat = {
       pandocMode: 'markdown',
       pandocExtensions: '',
@@ -220,136 +277,12 @@ class EditorPane extends React.Component<EditorPaneProps> {
       },
       docTypes: [kQuartoDocType]
     }
-    try {
-      return await Editor.create(this.parent!, context, format, { 
-        browserSpellCheck: true,
-        commenting: true,
-        outerScrollContainer: true 
-      });
-    } catch(e) {
-      this.errorAlert(e);
-      return undefined;
-    }
-  }
-
-
-
-  private async updateEditor() {
-    // set content (will no-op if prop change was from ourselves)
-    await this.setEditorContent(this.props.markdown);
-   
-    if (this.props.title !== this.editor!.getTitle()) {
-      this.editor!.setTitle(this.props.title);
-    }
-  }
-
-  private get editorDialogs(): EditorDialogs {
-    const dialogsImpl = this.editorDialogsRef.current!;
-    return editorDialogs(dialogsImpl);
-  }
-  private async setEditorContent(markdown: string) {
-    if (markdown !== this.editorMarkdown) {
-      this.editorMarkdown = markdown;
-      try {
-        await this.editor!.setMarkdown(markdown, this.panmirrorWriterOptions(), false);
-        this.props.setTitle(this.editor!.getTitle() || '');
-        this.onEditorOutlineChanged();
-        if (this.props.loading) {
-          this.props.setLoading(false);
-          this.editor!.focus();
-        }
-      } catch (error) {
-        this.errorAlert(error);
-      }
-    }
-  }
-
-  private async onEditorDocChanged() {
-    if (this.props.showMarkdown) {
-      this.saveMarkdown();
-    }
-
-    // set title into reduce
-    this.syncEditorTitle();
-  }
-
-  private syncEditorTitle() {
-     // set title into reduce
-     const title = this.editor!.getTitle();
-     this.props.setTitle(title || '');
-  }
-
-  private onEditorOutlineChanged() {
-    // set outline into redux
-    const outline = this.editor!.getOutline();
-    if (outline) {
-      this.props.setOutline(outline);
-    }
-  }
-
-  private async saveMarkdown() {
-    try {
-      // generate markdown (save a copy so we can ignore resulting update)
-      const markdown = await this.editor!.getMarkdown(this.panmirrorWriterOptions());
-      this.editorMarkdown = markdown.code;
-
-      // set markdown into redux
-      this.props.setMarkdown(markdown.code);
-    } catch (error) {
-      this.errorAlert(error);
-    }
-  }
-
-  private errorAlert(error: unknown) {
-    const message = error instanceof Error ? error.message : String(error);
-    this.editorDialogs.alert(message, t('error_alert_title'), kAlertTypeError);
-  }
-
-  private onEditorStateChanged() {
-    this.props.setSelection(this.editor!.getSelection());
-  }
-
-  private onEditorEvent<T>(event: EventType<T>, handler: (detail?: T) => void) {
-    this.editorEvents.push(this.editor!.subscribe(event, handler.bind(this)));
-  }
-
-  private showPandocWarnings() {
-    const pandocFormat = this.editor!.getPandocFormat();
-    const warnings = pandocFormat.warnings;
-    if (warnings.invalidFormat) {
-      // console.log('WARNING: invalid pandoc format ' + warnings.invalidFormat);
-    }
-    if (warnings.invalidOptions.length) {
-      // console.log(`WARNING: ${pandocFormat.baseName} does not support options: ${warnings.invalidOptions.join(', ')}`);
-    }
-  }
-
-  private panmirrorWriterOptions() {
-    return {
-      atxHeaders: true
-    };
-  }
+    return await Editor.create(parent, context, format, { 
+      browserSpellCheck: true,
+      commenting: true,
+      outerScrollContainer: true 
+    });
 }
 
+export default EditorPane;
 
-const mapStateToProps = (state: WorkbenchState) => {
-  return {
-    loading: state.editor.loading,
-    title: state.editor.title,
-    markdown: state.editor.markdown,
-    showMarkdown: state.prefs.showMarkdown,
-  };
-};
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const mapDispatchToProps = (dispatch: any) => {
-  return {
-    setLoading: (loading: boolean) => dispatch(setEditorLoading(loading)),
-    setMarkdown: (markdown: string) => dispatch(setEditorMarkdown(markdown)),
-    setTitle: (title: string) => dispatch(setEditorTitle(title)),
-    setOutline: (outline: EditorOutline) => dispatch(setEditorOutline(outline)),
-    setSelection: (selection: unknown) => dispatch(setEditorSelection(selection)),
-  };
-};
-
-export default withCommandManager(connect(mapStateToProps, mapDispatchToProps)(EditorPane));

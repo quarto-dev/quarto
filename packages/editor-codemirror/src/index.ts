77f0acb5b1f4a94daa1dcbb80732c493ee942300
J.J. Allaire <jj.allaire@gmail.com>
2022-12-13 11:08:24 -0500

    remove highlight demo

--- packages/editor-codemirror/src/index.ts
+++ packages/editor-codemirror/src/index.ts
@@ -26,9 +26,6 @@ import { arrowHandler } from "./utils";
 
 export const codeMirrorPluginKey = new PluginKey("codemirror");
 
-import { highlightDemo } from "./highlight";
-highlightDemo();
-
 export function codeMirrorExtension(
   codeViews: { [key: string]: CodeViewOptions })
 : ExtensionFn {

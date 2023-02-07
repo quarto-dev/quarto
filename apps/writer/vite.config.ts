27fe909ceb46fdf07ae6363b459ea3da8233a83a
J.J. Allaire <jj.allaire@gmail.com>
2023-02-07 10:03:09 -0500

    vite defines for writer

--- apps/writer/vite.config.ts
+++ apps/writer/vite.config.ts
@@ -6,6 +6,12 @@ const writerServerProxyTarget = {
 }
 
 export default defineConfig({
+  define: {
+    'process.env.DEBUG': '""',
+    'process.env.NODE_ENV': '"production"',
+    'process.env.TERM': '""',
+    'process.platform': '""'
+  },
   server: {
     proxy: {
       "/rpc": writerServerProxyTarget

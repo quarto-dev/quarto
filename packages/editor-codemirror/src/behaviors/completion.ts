f161628e7841d52d62d6244a6bb4c86e7e580705
J.J. Allaire <jj.allaire@gmail.com>
2023-02-17 08:23:46 -0500

    improved filtering/boosting

--- packages/editor-codemirror/src/behaviors/completion.ts
+++ packages/editor-codemirror/src/behaviors/completion.ts
@@ -41,16 +41,12 @@ import md from "markdown-it";
 
 import { editorLanguage } from "editor-core";
 
-import { codeViewCompletionContext } from "editor";
+import { CodeViewCompletionContext, codeViewCompletionContext } from "editor";
 
 import { Behavior, BehaviorContext } from ".";
 
 // TODO: types/icons
 // TODO: YAML and TeX completions
-// TODO: explore throttling
-// TODO: play w/ fuzzy matching
-// TODO: respect prefs / num chars
-
 
 export function completionBehavior(behaviorContext: BehaviorContext) : Behavior {
 
@@ -92,101 +88,128 @@ export function completionBehavior(behaviorContext: BehaviorContext) : Behavior 
             }
 
             // get completions
-            const completions = await behaviorContext.pmContext.ui.completion?.codeViewCompletions(cvContext);
-            if (context.aborted || !completions || completions.items.length == 0) {
-              return null;
-            }
-
-            // order completions
-            const haveOrder = !!completions.items?.[0].sortText;
-            if (haveOrder) {
-              completions.items = completions.items.sort((a, b) => {
-                if (a.sortText && b.sortText) {
-                  return a.sortText.localeCompare(b.sortText);
-                } else {
-                  return 0;
-                }
-              });  
-            }
-              
-          
-            // use order to create boost
-            const total = completions.items.length;
-            const boostScore = (index: number) => {
-              if (haveOrder) {
-                return -99 + Math.round(((total-index)/total) * 198);
-              } else {
-                return undefined;
-              }
-            }
-
-            // compute from
-            const itemFrom = (item: CompletionItem) => {
-               // compute from
-               return item.textEdit 
-                ? InsertReplaceEdit.is(item.textEdit) 
-                    ? context.pos - (item.textEdit.insert.end.character - item.textEdit.insert.start.character)
-                    : TextEdit.is(item.textEdit)
-                        ? context.pos - (item.textEdit.range.end.character - item.textEdit.range.start.character)
-                        : context.pos
-                : context.pos;
-            }
-
-            // return completions
-            return {
-              from: context.pos,
-              
-              options: completions.items
-                .filter(item => {
-                  
-                  // no text completions that aren't snippets
-                  if (item.kind === CompletionItemKind.Text &&
-                      item.insertTextFormat !== InsertTextFormat.Snippet) {
-                    return false;
-                  }
-                   
-                  // only return label prefix matches
-                  const replaceText = context.state.sliceDoc(itemFrom(item), context.pos).toLowerCase();
-                  return item.label.toLowerCase().includes(replaceText) ||
-                         (item.insertText && item.insertText.toLowerCase().includes(replaceText));
-                })
-                .map((item,index) : Completion => {
-                  return {
-                    label: item.label,
-                    detail: item.detail && !item.documentation ? item.detail : undefined,
-                    info: () : Node | null => {
-                      if (item.documentation) {
-                        return infoNodeForItem(item);   
-                      } else {
-                        return null;
-                      }
-                    },
-                    apply: (view: EditorView, completion: Completion, from: number) => {
-                      // compute from
-                      from = itemFrom(item);
-    
-                      // handle snippets
-                      const insertText = item.insertText || item.label;
-                      if (item.insertTextFormat === InsertTextFormat.Snippet) {
-                        const insertSnippet = snippet(insertText.replace(/\$(\d+)/g, "$${$1}"));
-                        insertSnippet(view, completion, from, context.pos);
-                      // normal completions
-                      } else {
-                        view.dispatch({
-                          ...insertCompletionText(view.state, insertText, from, context.pos),
-                          annotations: pickedCompletion.of(completion)
-                        })
-                      }
-                    },
-                    boost: boostScore(index)
-                  }
-                })
-              };
+            return getCompletions(context, cvContext, behaviorContext);
           }
         ]
       })
     ]
   }
+}
+
+async function getCompletions(
+  context: CompletionContext,
+  cvContext: CodeViewCompletionContext,
+  behaviorContext: BehaviorContext
+) : Promise<CompletionResult | null> {
+
+  const completions = await behaviorContext.pmContext.ui.completion?.codeViewCompletions(cvContext);
+  if (context.aborted || !completions || completions.items.length == 0) {
+    return null;
+  }
+
+  // order completions
+  const haveOrder = !!completions.items?.[0].sortText;
+  if (haveOrder) {
+    completions.items = completions.items.sort((a, b) => {
+      if (a.sortText && b.sortText) {
+        return a.sortText.localeCompare(b.sortText);
+      } else {
+        return 0;
+      }
+    });  
+  }
+    
+
+
+  // compute from
+  const itemFrom = (item: CompletionItem) => {
+      // compute from
+      return item.textEdit 
+      ? InsertReplaceEdit.is(item.textEdit) 
+          ? context.pos - (item.textEdit.insert.end.character - item.textEdit.insert.start.character)
+          : TextEdit.is(item.textEdit)
+              ? context.pos - (item.textEdit.range.end.character - item.textEdit.range.start.character)
+              : context.pos
+      : context.pos;
+  }
+
+  // use order to create boost
+  const total = completions.items.length;
+  const boostScore = (index: number) => {
+    
+    if (haveOrder) {
+       // compute replaceText
+      const item = completions.items[index];
+      const replaceText = context.state.sliceDoc(itemFrom(item), context.pos).toLowerCase();
+
+      // if the replaceText doesn't start with "." then bury items thatdo
+      if (!replaceText.startsWith(".") && item.label.startsWith(".")) {
+        return -99;
+      }
+
+      // only boost things that have a prefix match
+      if (item.label.toLowerCase().startsWith(replaceText) ||
+          (item.insertText && item.insertText.toLowerCase().startsWith(replaceText))) {
+        return -99 + Math.round(((total-index)/total) * 198);;
+      } else {
+        return -99;
+      }
+
+    } else {
+      return undefined;
+    }
+  }
+
+  // return completions
+  return {
+    from: context.pos,
+    
+    options: completions.items
+      .filter(item => {
+        
+        // no text completions that aren't snippets
+        if (item.kind === CompletionItemKind.Text &&
+            item.insertTextFormat !== InsertTextFormat.Snippet) {
+          return false;
+        }
+          
+        // only return label prefix matches
+        const replaceText = context.state.sliceDoc(itemFrom(item), context.pos).toLowerCase();
+        return item.label.toLowerCase().includes(replaceText) ||
+                (item.insertText && item.insertText.toLowerCase().includes(replaceText));
+      })
+      .map((item,index) : Completion => {
+        return {
+          label: item.label,
+          detail: item.detail && !item.documentation ? item.detail : undefined,
+          info: () : Node | null => {
+            if (item.documentation) {
+              return infoNodeForItem(item);   
+            } else {
+              return null;
+            }
+          },
+          apply: (view: EditorView, completion: Completion, from: number) => {
+            // compute from
+            from = itemFrom(item);
+
+            // handle snippets
+            const insertText = item.insertText || item.label;
+            if (item.insertTextFormat === InsertTextFormat.Snippet) {
+              const insertSnippet = snippet(insertText.replace(/\$(\d+)/g, "$${$1}"));
+              insertSnippet(view, completion, from, context.pos);
+            // normal completions
+            } else {
+              view.dispatch({
+                ...insertCompletionText(view.state, insertText, from, context.pos),
+                annotations: pickedCompletion.of(completion)
+              })
+            }
+          },
+          boost: boostScore(index)
+        }
+      })
+  };
 }
 
 function infoNodeForItem(item: CompletionItem) {
@@ -217,12 +240,14 @@ function infoNodeForItem(item: CompletionItem) {
         const mdDiv = document.createElement("div");
         mdDiv.innerHTML = html;
         // remove mdn links
-        for (const paraEl of mdDiv.querySelectorAll("p")) {
+        mdDiv.querySelectorAll("p").forEach(paraEl => {
           if (paraEl.childElementCount === 1 && 
               paraEl.firstElementChild?.tagName === "A") {
             paraEl.parentElement?.removeChild(paraEl);
           }
-        }
+        });
+        
+       
         infoDiv.appendChild(mdDiv);
       } else {
         infoDiv.appendChild(textDiv(item.documentation.value));

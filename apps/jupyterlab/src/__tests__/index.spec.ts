c9dc32d4a09b4c5fd31bcff9ec0d2dd23a7efcfe
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2023-03-22 18:20:46 -0700

    minimal mdit test in test suite

--- apps/jupyterlab/src/__tests__/index.spec.ts
+++ apps/jupyterlab/src/__tests__/index.spec.ts
@@ -3,68 +3,70 @@
  * @jest-environment-options {"url": "https://jestjs.io/"}
  */
 
-import {describe, test} from '@jest/globals';
+import {describe, test, expect} from '@jest/globals';
 import MarkdownIt, { Options } from 'markdown-it';
 import { toAst } from '../../src/ast/ast';
-import { MarkdownItPluginProvider } from '../../src/types';
-import { RenderedMarkdown } from '../../src/widgets';
 
-import plugins from '../../src/index';
+import sub from 'markdown-it-sub';
+import sup from 'markdown-it-sup';
+import deflist from 'markdown-it-deflist';
+import figures from 'markdown-it-implicit-figures';
+import footnotes from 'markdown-it-footnote';
+import tasklists from 'markdown-it-task-lists';
+import mermaid from '../plugins/mermaid';
+import attrs from 'markdown-it-attrs';
+import gridtables from '../plugins/gridtables';
+
+import { citationPlugin as cites } from '../plugins/cites';
+import { calloutPlugin as callouts } from '../plugins/callouts';
+import { divPlugin as divs } from '../plugins/divs';
+import { headingsPlugin as headings } from '../plugins/headings';
 
 // Provides resolved MarkdownIt options using the passed in options, the plugin
 // options, and default options.
-const resolveOptions = (widget: RenderedMarkdown, options: Options, providers: MarkdownItPluginProvider[]) => {
+const resolveOptions = (options: Options) => {
   // Build options table
   let allOptions: Options = {
     html: true,
     linkify: true,
     typographer: true,
   };
-  for (const plugin of providers) {
-    if (plugin.options) {
-      try {
-        // Add options for this plugin
-        allOptions = { ...allOptions, ...plugin.options(widget) };
-      } catch (err) {
-        console.warn(`Failed to get options from markdown-it plugin ${plugin.id}`, err);
-      }  
-    }
-  }
-
   return {
     ...allOptions,
-    ...options };
+    ...options 
+  };
 }
-
-// const _testMds = [
-//   '# Hello\n\n## Subsection\n\n1. one\n2. two\n3. three\n\nWow.',
-//   '[foo](Hello)',
-//   '[foo]{#id .class attribute=\'value\'}',
-//   'This is a paragraph with a [link](http://example.com).',
-//   '::: container\n\nThis is a container\n\n:::'
-// ];
 
 describe('sum module', () => {
   test('adds 1 + 2 to equal 3', () => {
     const str = '# Hi there\n\nThis is a test.';
 
-    const providers: MarkdownItPluginProvider[] = [];
-    for (const plugin of plugins) {
-      if (plugin?.provider) {
-        providers.push(plugin.provider);
-      }
-    }
-
     // no current plugin uses this parameter
-    const options = resolveOptions(
-      undefined as unknown as RenderedMarkdown,
-      {},
-      providers
-    );
+    const options = resolveOptions({});
 
-    let md = new MarkdownIt('default', options);
+    let md = new MarkdownIt('default', options)
+      .use(sub)
+      .use(sup)
+      .use(deflist)
+      .use(figures, { figcaption: true })
+      .use(footnotes)
+      .use(tasklists)
+      .use(cites)
+      .use(mermaid)
+      .use(callouts)
+      .use(attrs)
+      .use(divs)
+      .use(headings)
+      .use(gridtables);
+    
     console.log(toAst(md.parse(str, {})));
+    expect(true).toBe(true);
   });
 });
 
 
+describe('nothing', () => {
+  test('nothing', () => {
+    expect(true).toBe(true);
+  });
+});

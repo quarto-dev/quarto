9c9eaeb2a6cfd635f793f54092a2155098840653
J.J. Allaire <jj.allaire@gmail.com>
2023-03-25 11:16:02 -0400

    use ts-node for zotero sync

--- apps/zotero-sync/src/index.ts
+++ apps/zotero-sync/src/index.ts
@@ -1,12 +1,11 @@
 /// <reference types="node" />/
 
 
+import { setInterval } from "timers";
 
-
-import { readFileSync } from "fs";
+console.log("here we go again!");
 
 
 // ensure that the deno runtime won't exit b/c of the event queue being empty
 setInterval(() => { /* */ }, 1000);
-
 

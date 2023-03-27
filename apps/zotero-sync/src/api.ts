a9ce0ab50a1b3cbaaa44d43d2df5d714536ca836
J.J. Allaire <jj.allaire@gmail.com>
2023-03-27 13:41:41 -0400

    version awareness for zotero api

--- apps/zotero-sync/src/api.ts
+++ apps/zotero-sync/src/api.ts
@@ -1,35 +1,30 @@
-
-
+/*
+ * api.ts
+ *
+ * Copyright (C) 2022 by Posit Software, PBC
+ *
+ * Unless you have received this program directly from Posit Software pursuant
+ * to the terms of a commercial license agreement with Posit Software, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
 
 import fetch from "cross-fetch";
 
-import { handleResponseWithStatus } from "core-node";
 import { CSL } from "editor-types";
 
-export interface ZoteroApi {
-  user() : Promise<User>;
-
-  groupVersions() : Promise<ObjectVersions>;
-  group(groupID: number) : Promise<VersionedResponse<Group>>;
-
-  collectionVersions(since: number, groupID?: number) : Promise<VersionedResponse<ObjectVersions>>;
-  collections(keys: string[], groupID?: number) : Promise<VersionedResponse<Collection[]>>;
- 
-  itemVersions(since: number, groupId?: number) : Promise<VersionedResponse<ObjectVersions>>;
-  items(keys: string[], groupID?: number) : Promise<VersionedResponse<Item[]>>;
-
-}
-
-export type VersionedResponse<T> = { data: T, version: number | null };
-
-export interface GroupAccess {
-  library: boolean;
-  write: boolean;
+export interface Library {
+  type: "user" | "group";
+  id: number;
 }
 
 export interface User {
   key: string;
-  userID: string;
+  userID: number;
   username: string;
   displayName: string;
   access: {
@@ -41,6 +36,11 @@ export interface User {
     },
     groups: { [key: string]: GroupAccess }
   }
+}
+
+export interface GroupAccess {
+  library: boolean;
+  write: boolean;
 }
 
 export interface Group {
@@ -74,105 +74,133 @@ export interface Item {
 
 export type ObjectVersions = { [objectId: string]: number };
 
+export type VersionedResponse<T> = { data: T, version: number | null } | null;
+
+export interface ZoteroApi {
+  user() : Promise<User>;
+
+  groupVersions(userID: number) : Promise<ObjectVersions>;
+  group(groupID: number) : Promise<VersionedResponse<Group>>;
+
+  collectionVersions(library: Library, since: number) : Promise<VersionedResponse<ObjectVersions>>;
+  collections(library: Library, keys: string[], since: number) : Promise<VersionedResponse<Collection[]>>;
+ 
+  itemVersions(library: Library, since: number) : Promise<VersionedResponse<ObjectVersions>>;
+  items(library: Library, keys: string[], since: number) : Promise<VersionedResponse<Item[]>>;
+}
+
 export function zoteroApi(key: string) : ZoteroApi {
 
-  const kZoteroApiUrl = "https://api.zotero.org";
-  const kUserInfoPath = "/keys/current";
-  const kUserIDToken = "<userID>";
-
-  // get user id on demand and cache it
-  let userID: string | undefined;
-  const user = async () => {
-    const data = (await zoteroRequest<User>(kUserInfoPath)).data;
-    userID = data.userID;
-    return data;
-  }
-
-  // generic request handler
-  const zoteroRequest = async <T>(path: string) : Promise<VersionedResponse<T>> => {
-    // ensure userID if required
-    if (path.includes(kUserIDToken) && !userID) {
-      // caches userID as a side effect
-      await user();
-    }
-    if (userID) {
-      path = path.replace(kUserIDToken, userID);
-    }
-    const url = `${kZoteroApiUrl}${path}`;
-    const req = () => fetch(url, {
-      headers: {
-        "Zotero-API-Version": "3",
-        "Zotero-API-Key": key
-      }
-    });
-    const response = await handleResponseWithStatus<T>(req);
-    if (response.status === 200 && response.message) {
-      const version = Number(response.headers?.get("Last-Modified-Version"));
-      return {
-        data: response.message,
-        version: version || null 
-      }
-    } else {
-      throw new Error(response.error);
-    }
-  }
-
-  const objectPrefix = (groupID?: number) => {
-    return  groupID !== undefined ? `/groups/${groupID}` : `/users/<userID>`;
+  const objectPrefix = (library: Library) => {
+    return `/${library.type}s/${library.id}`;
   };
 
   return {
-    user,
-
-    groupVersions: async () => {
-      const response =  await zoteroRequest<ObjectVersions>("/users/<userID>/groups?format=versions");
-      return response.data;
+    user: () => {
+      return zoteroRequest<User>(key, "/keys/current");
     },
 
-    group: async (groupID: number) => {
-      const response = await zoteroRequest<Group>(`/groups/${groupID}`);
-      return response;
+    groupVersions: (userID: number) => {
+      return zoteroRequest<ObjectVersions>(key, `/users/${userID}/groups?format=versions`);
     },
 
-    collectionVersions: async (since: number, groupID?: number) : Promise<VersionedResponse<ObjectVersions>> => {
-      const prefix = objectPrefix(groupID);
-      const response = await zoteroRequest<ObjectVersions>(`${prefix}/collections?since=${since}&format=versions`);
-      return response;
+    group: (groupID: number) => {
+      return zoteroVersionedRequest<Group>(key, `/groups/${groupID}`, 0);
     },
 
-    collections: async (keys: string[], groupID?: number) : Promise<VersionedResponse<Collection[]>> => {
-      const prefix = objectPrefix(groupID);
+    collectionVersions: (library: Library, since: number) => {
+      const prefix = objectPrefix(library);
+      return zoteroVersionedRequest<ObjectVersions>(key, `${prefix}/collections?since=${since}&format=versions`, since);
+    },
+
+    collections: async (library: Library, keys: string[], since: number) => {
+      const prefix = objectPrefix(library);
       const query = `/collections?collectionKey=${keys.join(',')}`;
-      const response = await zoteroRequest<Array<{ data: Collection }>>(`${prefix}${query}`);
-      return {
-        data: response.data.map(x => x.data),
-        version: response.version
-
+      const response = await zoteroVersionedRequest<Array<{ data: Collection }>>(key, `${prefix}${query}`, since);
+      if (response) {
+        return {
+          data: response.data.map(x => x.data),
+          version: response.version
+        }
+      } else {
+        return null;
       }
     },
 
-    itemVersions: async (since: number, groupID?: number) : Promise<VersionedResponse<ObjectVersions>> => {
-      const prefix = objectPrefix(groupID);
+    itemVersions: (library: Library, since: number) => {
+      const prefix = objectPrefix(library);
       const query = `/items?since=${since}&itemType=-attachment&format=versions&includeTrashed=1`;
-      const response = await zoteroRequest<ObjectVersions>(`${prefix}${query}`);
-      return response;
+      return zoteroVersionedRequest<ObjectVersions>(key, `${prefix}${query}`, since);
     },
 
-    items: async (keys: string[], groupID?: number) : Promise<VersionedResponse<Item[]>> => {
-      const prefix = objectPrefix(groupID);
+    items: (library: Library, keys: string[], since: number) => {
+      const prefix = objectPrefix(library);
       const query = `/items?itemKey=${keys.join(',')}&format=json&include=csljson,data&includeTrashed=1`;
-      const response = await zoteroRequest<Item[]>(`${prefix}${query}`);
-      return response;
+      return zoteroVersionedRequest<Item[]>(key, `${prefix}${query}`, since);
     }
-
-
-
-   
-
-
-
-
   }
-
-
 }
+
+interface ZoteroResponse<T> {
+  status: number;
+  statusText: string;
+  headers: Headers | null;
+  message: T | null;
+}
+
+// normal request handler
+const zoteroRequest = async <T>(key: string, path: string) : Promise<T> => {
+  const response = await zoteroFetch<T>(key, path);
+  if (response.status === 200 && response.message) {
+    return response.message;
+  } else {
+    throw new Error(response.statusText || "Unknown error");
+  }
+}
+
+const zoteroVersionedRequest = async <T>(key: string, path: string, since: number) : Promise<VersionedResponse<T>> => {
+  const response = await zoteroFetch<T>(key, path, { ["If-Modified-Since-Version"]: String(since) });
+  if (response.status === 200 && response.message) {
+    const version = Number(response.headers?.get("Last-Modified-Version"));
+    return {
+      data: response.message,
+      version: version || null 
+    }
+  } else if (response.status === 304) {
+    return null;
+  } else {
+    throw new Error(response.statusText || "Unknown error");
+  }
+}
+
+const zoteroFetch = async <T>(
+  key: string, 
+  path: string, 
+  headers = {} as Record<string,string>
+) : Promise<ZoteroResponse<T>> => {
+  try {
+    const url = `https://api.zotero.org${path}`;
+    const response = await fetch(url, {
+      headers: {
+        "Zotero-API-Version": "3",
+        "Zotero-API-Key": key,
+        ...headers
+      }
+    });
+    return {
+      status: response.status,
+      statusText: response.statusText,
+      headers: response.headers,
+      message: response.ok ? await response.json() as T  : null,
+    }
+  } catch(error) {
+    const message = error instanceof Error ? error.message : JSON.stringify(error);
+    return {
+      status: message.includes("ENOTFOUND") ? 503 : 500,
+      statusText: `Error: ${message}`,
+      headers: null,
+      message: null,
+    }
+  }
+}
+
--- apps/zotero-sync/src/index.ts
+++ apps/zotero-sync/src/index.ts
@@ -1,8 +1,21 @@
 /// <reference types="node" />/
-
+/*
+ * index.ts
+ *
+ * Copyright (C) 2022 by Posit Software, PBC
+ *
+ * Unless you have received this program directly from Posit Software pursuant
+ * to the terms of a commercial license agreement with Posit Software, then
+ * this program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
 
 import { setInterval } from "timers";
-import { zoteroApi } from "./api";
+import { Library, zoteroApi } from "./api";
 
 const dump = (obj: unknown) => {
   console.log(JSON.stringify(obj, undefined, 2))
@@ -17,20 +30,31 @@ const zotero = zoteroApi("ZL68OPrdYJdKknDdlytHMcQc");
 
 
 zotero.user().then(async (user) => {
-  dump(user);
+  //dump(user);
+
   /*
-  dump(await zotero.groupVersions());
-
+  dump(await zotero.groupVersions(user.userID));
   dump(await zotero.group(2226282));
-
-  const collections = await zotero.collectionVersions(0);
-  dump(collections);
-  dump(await zotero.collections(Object.keys(collections.data)));
   */
 
-  const items = await zotero.itemVersions(0);
+  /*
+  const groupLibrary: Library = { type: "group", id: 2558114 };
+  const userLibrary: Library = { type: "user", id: user.userID };
+  */
+ 
+  /*
+  const collections = await zotero.collectionVersions(groupLibrary, 3);
+  dump(collections);
+  dump(await zotero.collections(groupLibrary, ["57YSFW7C"], 3));
+  */
+
+  /*
+  const items = await zotero.itemVersions(userLibrary, 0);
   dump(items);
-  dump(await zotero.items(Object.keys(items.data)));
+  if (items) {
+    dump(await zotero.items(userLibrary, Object.keys(items.data).slice(0, 1), 154));
+  }
+  */
 });
 
 

742478ed8011cf01c0e8317d5c96de223dcbfff5
J.J. Allaire <jj.allaire@gmail.com>
2023-04-29 16:07:01 -0400

    implement getLibraryNames and getActiveCollectionSpecs

--- packages/editor-server/src/core/zotero/local/source.ts
+++ packages/editor-server/src/core/zotero/local/source.ts
@@ -26,7 +26,6 @@ export function zoteroLocalCollectionSource(dataDir?: string) : ZoteroCollection
  
   // resolve data dir
   dataDir = zoteroDataDir(dataDir);
-  
   return {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     async getCollections(collections: string[], cached: ZoteroCollectionSpec[]) : Promise<ZoteroResult> {
@@ -54,32 +53,83 @@ export function zoteroLocalCollectionSource(dataDir?: string) : ZoteroCollection
     },
 
     async getLibraryNames(): Promise<ZoteroResult> {
-      try {  
-        
-
-
-        return {
-          status: "ok",
-          message: [],
-          warning: '',
-          error: ''
+      if (dataDir) {
+        try {  
+          const libraries = await withZoteroDb<string[]>(dataDir, async (db: Database) => {
+            return getCollections(db)
+              .filter(collection => !collection.parentKey)
+              .map(collection => collection.name)
+          });
+          return {
+            status: "ok",
+            message: libraries,
+            warning: '',
+            error: ''
+          }
+        } catch(error) {
+          return handleZoteroError(error);
         }
-      } catch(error) {
-        return handleZoteroError(error);
+      } else {
+        return zoteroResultEmpty();
       }
+      
     },
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     async getActiveCollectionSpecs(collections: string[]): Promise<ZoteroResult> {
-      try { 
-        return {
-          status: 'ok',
-          message: [],
-          warning: '',
-          error: ''
+      if (dataDir) {
+
+        try {  
+          const specs = await withZoteroDb<ZoteroCollectionSpec[]>(dataDir, async (db: Database) => {
+            
+            // get all collections
+            const allCollections = getCollections(db);
+            
+            // find the parent of a given collection
+            const findParentSpec = (spec: ZoteroCollectionSpec) => {
+              if (spec.parentKey.length > 0) {
+                return allCollections.find(coll => coll.key === spec.parentKey);
+              } else {
+                return undefined;
+              }
+            }
+
+            // filter on the passed collections if need be
+            if (collections.length > 0) {
+
+              return allCollections.filter(spec => {
+                // find the top-level library of the spec (when the loop terminates
+                // the targetSpec will be the library spec)
+                let targetSpec = spec;
+                for (;;)
+                {
+                  const parentSpec = findParentSpec(targetSpec);
+                  if (!parentSpec) {
+                    break;
+                  } else {
+                    targetSpec = parentSpec;
+                  }
+                }
+
+                // see if that library is in the list of passed collections
+                return collections.includes(targetSpec.name);
+              })
+            } else {
+              return allCollections;
+            }
+          });
+
+          return {
+            status: "ok",
+            message: specs,
+            warning: '',
+            error: ''
+          }
+        } catch(error) {
+          return handleZoteroError(error);
         }
-      } catch(error) {
-        return handleZoteroError(error)
+      } else {
+        return zoteroResultEmpty();
       }
     }
   };
@@ -103,88 +153,62 @@ function zoteroResultEmpty(message = []) : ZoteroResult {
   }
 }
 
-/*
-ZoteroCollectionSpecs getCollections(boost::shared_ptr<database::IConnection> pConnection, bool librariesOnly = false)
-{
-   std::string librariesSql = R"(
-                              SELECT
-                                  CAST(libraries.libraryID as text) as collectionKey,
-                                  IFNULL(groups.name, 'My Library') as collectionName,
-                                  NULL as parentCollectionKey,
-                                  IFNULL(strftime('%s', MAX(MAX(items.clientDateModified), MAX(collections.clientDateModified))), '0') AS version
-                              FROM
-                                  libraries
-                                  left join items on libraries.libraryID = items.libraryID
-                                  left join collections on libraries.libraryID = collections.libraryID
-                                  join itemTypes on items.itemTypeID = itemTypes.itemTypeID
-                                  left join deletedItems on items.itemId = deletedItems.itemID
-                                  left join groups as groups on libraries.libraryID = groups.libraryID
-                              WHERE
-                                  libraries.type in ('user', 'group')
-                              AND
-                                  itemTypes.typeName <> 'attachment'
-                              AND
-                                  itemTypes.typeName <> 'note'
-                              AND
-                                  deletedItems.dateDeleted IS NULL
-                              GROUP
-                                  BY libraries.libraryID
-                          )";
+function getCollections(db: Database, librariesOnly = false) : ZoteroCollectionSpec[] {
 
-   std::string collectionsSql = R"(
-                                SELECT
-                                    collections.key as collectionKey,
-                                    collections.collectionName as collectionName,
-                                    IFNULL(parentCollections.key, libraries.libraryId) as parentCollectionKey,
-                                    IFNULL(strftime('%s', MAX(MAX(items.clientDateModified), MAX(collections.clientDateModified))), '0') AS version
-                                FROM
-                                    collections
-                                    join libraries on libraries.libraryID = collections.libraryID
-                                    left join collectionItems on collections.collectionID = collectionItems.collectionID
-                                    left join items on collectionItems.itemID = items.itemID
-                                    left join itemTypes on items.itemTypeID = itemTypes.itemTypeID
-                                    left join collections as parentCollections on collections.parentCollectionID = parentCollections.collectionID
-                                    left join groups as groups on libraries.libraryID = groups.libraryID
-                                GROUP BY
-                                    collections.key
-                            )";
+  const librariesSql = `
+    SELECT
+        CAST(libraries.libraryID as text) as collectionKey,
+        IFNULL(groups.name, 'My Library') as collectionName,
+        NULL as parentCollectionKey,
+        IFNULL(strftime('%s', MAX(MAX(items.clientDateModified), MAX(collections.clientDateModified))), '0') AS version,
+        libraries.version as dbVersion
+    FROM
+        libraries
+        left join items on libraries.libraryID = items.libraryID
+        left join collections on libraries.libraryID = collections.libraryID
+        join itemTypes on items.itemTypeID = itemTypes.itemTypeID
+        left join deletedItems on items.itemId = deletedItems.itemID
+        left join groups as groups on libraries.libraryID = groups.libraryID
+    WHERE
+        libraries.type in ('user', 'group')
+    AND
+        itemTypes.typeName <> 'attachment'
+    AND
+        itemTypes.typeName <> 'note'
+    AND
+        deletedItems.dateDeleted IS NULL
+    GROUP
+        BY libraries.libraryID
+  `.trim();
 
-    // If this is libraries only, just read the libraries, otherwise
-    // union the library and collection SQL
-    std::string sql = librariesSql;
-    if (!librariesOnly) {
-        sql = boost::str(boost::format("%1% UNION %2%") % librariesSql % collectionsSql);
+  const collectionsSql = `
+    SELECT
+        collections.key as collectionKey,
+        collections.collectionName as collectionName,
+        IFNULL(parentCollections.key, libraries.libraryId) as parentCollectionKey,
+        IFNULL(strftime('%s', MAX(MAX(items.clientDateModified), MAX(collections.clientDateModified))), '0') AS version,
+        collections.version as dbVersion
+    FROM
+        collections
+        join libraries on libraries.libraryID = collections.libraryID
+        left join collectionItems on collections.collectionID = collectionItems.collectionID
+        left join items on collectionItems.itemID = items.itemID
+        left join itemTypes on items.itemTypeID = itemTypes.itemTypeID
+        left join collections as parentCollections on collections.parentCollectionID = parentCollections.collectionID
+        left join groups as groups on libraries.libraryID = groups.libraryID
+    GROUP BY
+        collections.key
+  `.trim();
+
+  // If this is libraries only, just read the libraries, otherwise union the library and collection SQL
+  const sql = librariesOnly ? librariesSql : `${librariesSql} UNION ${collectionsSql}`;
+  return (db.all(sql) as Array<Record<string,string>>).map(row => {
+    const version = parseFloat(row["version"]) || parseFloat(row["dbVersion"]) || 0;
+    return {
+      name: String(row["collectionName"]),
+      version,
+      key: String(row["collectionKey"] || "-1"),
+      parentKey: String(row["parentCollectionKey"] || "")
     }
-
-   ZoteroCollectionSpecs specs;
-   Error error = execQuery(pConnection, sql, [&specs](const database::Row& row) {
-
-      // had this issue: https://github.com/rstudio/rstudio/issues/8861
-      // perhaps a corrupted collection or a collection using an older
-      // schema that was unversioned?
-      try
-      {
-         ZoteroCollectionSpec spec;
-         spec.name = row.get<std::string>("collectionName");
-         spec.key = readString(row, "collectionKey", "-1");
-
-         std::string versionStr = readString(row, "version", "0");
-         spec.version = safe_convert::stringTo<double>(versionStr, 0);
-
-         const soci::indicator indicator = row.get_indicator("parentCollectionKey");
-         if (indicator == soci::i_ok)
-         {
-            // If the parent key is not null, this is a child collection
-            spec.parentKey = row.get<std::string>("parentCollectionKey");
-         }
-         specs.push_back(spec);
-      }
-      CATCH_UNEXPECTED_EXCEPTION
-   });
-
-   if (error)
-      LOG_ERROR(error);
-
-   return specs;
+  });  
 }
-*/

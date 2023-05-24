1aa528c79737eb1785a9ef3cfe82c5001a09c3d7
J.J. Allaire <jj.allaire@gmail.com>
2023-05-23 20:01:39 -0400

    import yaml front matter partitioner

--- packages/quarto-core/src/metadata.ts
+++ packages/quarto-core/src/metadata.ts
@@ -19,6 +19,7 @@ import fs from "node:fs";
      
 import * as yaml from "js-yaml";
 import { ExecFileSyncOptions } from "child_process";
+import { lines } from "core";
 
 export function parseFrontMatterStr(str: string) {
   str = str.replace(/---\s*$/, "");
@@ -26,6 +27,37 @@ export function parseFrontMatterStr(str: string) {
     return yaml.load(str);
   } catch (error) {
     return undefined;
+  }
+}
+
+const kRegExBeginYAML = /^---[ \t]*$/;
+const kRegExEndYAML = /^(?:---|\.\.\.)([ \t]*)$/;
+
+export function partitionYamlFrontMatter(
+  markdown: string,
+): { yaml: string; markdown: string } | null {
+  // if there are are less than 3 lines or the first line isn't yaml then return null
+  const mdLines = lines(markdown.trimLeft());
+  if (mdLines.length < 3 || !mdLines[0].match(kRegExBeginYAML)) {
+    return null;
+    // if the second line is empty or has a --- then no go
+  } else if (
+    mdLines[1].trim().length === 0 || mdLines[1].match(kRegExEndYAML)
+  ) {
+    return null;
+  } else {
+    // if there is no end yaml position then return null
+    const endYamlPos = mdLines.findIndex((line, index) =>
+      index > 0 && line.match(kRegExEndYAML)
+    );
+    if (endYamlPos === -1) {
+      return null;
+    } else {
+      return {
+        yaml: mdLines.slice(0, endYamlPos + 1).join("\n"),
+        markdown: "\n" + mdLines.slice(endYamlPos + 1).join("\n"),
+      };
+    }
   }
 }
 

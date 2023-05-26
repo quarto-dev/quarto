4e42efab6b95dedfc7788b23a30bcab57671cc1c
J.J. Allaire <jj.allaire@gmail.com>
2023-05-26 08:26:24 -0400

    update todo

--- apps/lsp/src/index.ts
+++ apps/lsp/src/index.ts
@@ -18,6 +18,7 @@
 
 // TODO: test and tweak all of the features, updating changelog as required
 
+// TODO: see if we can move more stuff into the lsp (highlight, code lens, etc.)
 // TODO: see how _extensions plays in extension projects (check readonly?)
 
 import path from "node:path"

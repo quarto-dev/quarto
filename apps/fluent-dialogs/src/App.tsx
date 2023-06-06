4f2963958781fcf64cd6901ab6530edef1a8f51a
J.J. Allaire <jj.allaire@gmail.com>
2023-06-06 15:06:35 -0400

    basic dialog with autoFocus

--- apps/fluent-dialogs/src/App.tsx
+++ apps/fluent-dialogs/src/App.tsx
@@ -13,18 +13,72 @@
  *
  */
 
+
+import {
+  Dialog,
+  DialogTrigger,
+  DialogSurface,
+  DialogTitle,
+  DialogContent,
+  DialogBody,
+  DialogActions,
+  Button,
+  Input,
+  Label,
+  makeStyles,
+} from "@fluentui/react-components";
 import React from 'react';
 
+import { FluentProvider, webLightTheme } from '@fluentui/react-components';
 
+const useStyles = makeStyles({
+  content: {
+    display: "flex",
+    flexDirection: "column",
+    rowGap: "10px",
+  },
+});
 
 const App: React.FC = () => {
-
-  // render
+  const styles = useStyles();
+  const handleSubmit = (ev: React.FormEvent) => {
+    ev.preventDefault();
+    alert("form submitted!");
+  };
   return (
-    <div>My App</div>
+    <FluentProvider theme={webLightTheme}>
+      <Dialog modalType="modal">
+        <DialogTrigger disableButtonEnhancement>
+          <Button>Open formulary dialog</Button>
+        </DialogTrigger>
+        <DialogSurface aria-describedby={undefined}>
+          <form onSubmit={handleSubmit}>
+            <DialogBody>
+              <DialogTitle>Dialog title</DialogTitle>
+              <DialogContent className={styles.content}>
+                <Label required htmlFor={"email-input"}>
+                  Email input
+                </Label>
+                <Input required type="email" id={"email-input"} autoFocus={true}/>
+                <Label required htmlFor={"password-input"}>
+                  Password input
+                </Label>
+                <Input required type="password" id={"password-input"} />
+              </DialogContent>
+              <DialogActions>
+                <DialogTrigger disableButtonEnhancement>
+                  <Button appearance="secondary">Close</Button>
+                </DialogTrigger>
+                <Button type="submit" appearance="primary">
+                  Submit
+                </Button>
+              </DialogActions>
+            </DialogBody>
+          </form>
+        </DialogSurface>
+      </Dialog>
+    </FluentProvider>
   );
-
-
 }
 
 export default App;

c9233ce7889b949853b4e6c7b6ea2948f3b40fa8
Lionel Henry <lionel.hry@proton.me>
2025-07-03 11:07:27 +0200

    Build LSP directly in destination so source maps are correct

--- apps/lsp/build.ts
+++ apps/lsp/build.ts
@@ -23,13 +23,13 @@ const nodeSqlLiteWasm = '../../node_modules/node-sqlite3-wasm/dist/*.wasm';
 
 runBuild({
   entryPoints: ['./src/index.ts'],
-  outfile: './dist/lsp.js',
+  outfile: '../vscode/out/lsp/lsp.js',
   assets: [
-    { from: [nodeSqlLiteWasm], to: './dist/' },
-    { from: ['./src/run.js'], to: './dist' },
-    { from: ['../../packages/editor-server/src/resources/**'], to: './dist/resources/' },
-    { from: ['../../packages/quarto-core/src/resources/**'], to: './dist/resources/' },
-    { from: ['./dist/**'], to: ['../vscode/out/lsp/'] }],
+    { from: [nodeSqlLiteWasm], to: '../vscode/out/lsp/' },
+    { from: ['./src/run.js'], to: '../vscode/out/lsp' },
+    { from: ['../../packages/editor-server/src/resources/**'], to: '../vscode/out/lsp/resources/' },
+    { from: ['../../packages/quarto-core/src/resources/**'], to: '../vscode/out/lsp/resources/' }
+  ],
   minify: !dev,
   dev
 })

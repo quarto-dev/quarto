80ad154893bfc62f9941a070da78017b72c4eb78
Julia Silge <julia.silge@gmail.com>
2025-07-09 20:48:27 -0600

    First draft of simple extension test

---
+++ apps/vscode/.vscode-test.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from '@vscode/test-cli';
+
+export default defineConfig([
+  {
+    files: 'out/test/**/*.test.js',
+    workspaceFolder: 'test/examples',
+  },
+]);
---
+++ apps/vscode/src/test/toggle.test.ts
@@ -0,0 +1,20 @@
+import * as vscode from "vscode";
+import * as assert from "assert";
+import { isQuartoDoc } from "../core/doc";
+import { VisualEditorProvider } from "../providers/editor/editor";
+
+suite("Toggle editors", () => {
+  test("Switch from source to visual", async () => {
+    await vscode.commands.executeCommand("vscode.open", "hello.qmd");
+    const editor = vscode.window.activeTextEditor;
+    assert.strictEqual(isQuartoDoc(editor?.document), true);
+    assert.strictEqual(editor?.document.languageId, "python");
+
+    const beforeSwitchingEditor = VisualEditorProvider.activeEditor();
+    assert.strictEqual(beforeSwitchingEditor, undefined);
+
+    await vscode.commands.executeCommand("quarto.editInVisualMode");
+    const afterSwitchingEditor = VisualEditorProvider.activeEditor();
+    assert.strictEqual(afterSwitchingEditor?.textEditor, undefined);
+  });
+});

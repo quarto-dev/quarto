324935b9416e2ebbbabd295c84b783f2dcdc7730
elliot <elliot.evans@posit.co>
2025-07-22 14:31:11 -0400

    Add tests and test support commands for visual editor

--- apps/vscode/src/test/test-utils.ts
+++ apps/vscode/src/test/test-utils.ts
@@ -1,4 +1,5 @@
 import * as path from "path";
+import * as vscode from "vscode";
 
 
 /**
@@ -15,4 +16,33 @@ export const WORKSPACE_PATH = path.join(TEST_PATH, "examples");
 
 export function exampleWorkspacePath(file: string): string {
   return path.join(WORKSPACE_PATH, file);
+}
+export function exampleWorkspaceOutPath(file: string): string {
+  return path.join(WORKSPACE_PATH, 'examples-out', file);
+}
+
+export function wait(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export async function copyFile(
+  sourcePath: string,
+  destPath: string,
+): Promise<boolean> {
+  try {
+    const wsedit = new vscode.WorkspaceEdit();
+    const data = await vscode.workspace.fs.readFile(
+      vscode.Uri.file(sourcePath)
+    );
+    const destFileUri = vscode.Uri.file(destPath);
+    wsedit.createFile(destFileUri, { ignoreIfExists: true });
+
+    await vscode.workspace.fs.writeFile(destFileUri, data);
+
+    let isDone = await vscode.workspace.applyEdit(wsedit);
+    if (isDone) return true;
+    else return false;
+  } catch (err) {
+    return false;
+  }
 }

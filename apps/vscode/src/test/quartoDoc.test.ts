ec41257adc86095e7803233b9a1ed8b5b2bd59e4
elliot <elliot.evans@posit.co>
2025-07-23 15:42:24 -0400

    Properly skip tests

--- apps/vscode/src/test/quartoDoc.test.ts
+++ apps/vscode/src/test/quartoDoc.test.ts
@@ -6,14 +6,14 @@ import { extension } from "./extension";
 
 const APPROX_TIME_TO_OPEN_VISUAL_EDITOR = 1600;
 
-const quartoBasicsSuite = suite("Quarto basics", () => {
+suite("Quarto basics", function () {
   // Before we run any tests, we should copy any files that get edited in the tests to file under `exampleWorkspaceOutPath`
-  suiteSetup(async () => {
+  suiteSetup(async function () {
     const didCopyFile = await copyFile(exampleWorkspacePath('hello.qmd'), exampleWorkspaceOutPath('hello.qmd'));
     assert.ok(didCopyFile);
   });
 
-  test("Can open a Quarto document", async () => {
+  test("Can open a Quarto document", async function () {
     const doc = await vscode.workspace.openTextDocument(exampleWorkspaceOutPath("hello.qmd"));
     const editor = await vscode.window.showTextDocument(doc);
 
@@ -21,41 +21,44 @@ const quartoBasicsSuite = suite("Quarto basics", () => {
     assert.strictEqual(isQuartoDoc(editor?.document), true);
   });
 
-  // don't run these in CI for now because we haven't figured out how to get the LSP to start
-  if (!process.env['CI']) {
-    // Note: the following tests may be flaky. They rely on waiting estimated amounts of time for commands to complete.
-    test("Can edit in visual mode", async () => {
-      const doc = await vscode.workspace.openTextDocument(exampleWorkspaceOutPath("hello.qmd"));
-      const editor = await vscode.window.showTextDocument(doc);
+  // Note: the following tests may be flaky. They rely on waiting estimated amounts of time for commands to complete.
+  test("Can edit in visual mode", async function () {
+    // don't run this in CI for now because we haven't figured out how to get the LSP to start
+    if (process.env['CI']) this.skip();
 
-      // manually confirm visual mode so dialogue pop-up doesn't show because dialogues cause test errors
-      // and switch to visual editor
-      await vscode.commands.executeCommand("quarto.test_setkVisualModeConfirmedTrue");
-      await wait(300); // It seems necessary to wait around 300ms for this command to be done.
-      await vscode.commands.executeCommand("quarto.editInVisualMode");
-      await wait(APPROX_TIME_TO_OPEN_VISUAL_EDITOR);
+    const doc = await vscode.workspace.openTextDocument(exampleWorkspaceOutPath("hello.qmd"));
+    const editor = await vscode.window.showTextDocument(doc);
 
-      assert.ok(await vscode.commands.executeCommand("quarto.test_isInVisualEditor"));
-    });
-    // Note: this test runs after the previous test, so `hello.qmd` has already been touched by the previous
-    //       test. That's okay for this test, but could cause issues if you expect a qmd to look how it
-    //       does in `/examples`.
-    test("Roundtrip doesn't change hello.qmd", async () => {
-      const doc = await vscode.workspace.openTextDocument(exampleWorkspaceOutPath("hello.qmd"));
-      const editor = await vscode.window.showTextDocument(doc);
+    // manually confirm visual mode so dialogue pop-up doesn't show because dialogues cause test errors
+    // and switch to visual editor
+    await vscode.commands.executeCommand("quarto.test_setkVisualModeConfirmedTrue");
+    await wait(300); // It seems necessary to wait around 300ms for this command to be done.
+    await vscode.commands.executeCommand("quarto.editInVisualMode");
+    await wait(APPROX_TIME_TO_OPEN_VISUAL_EDITOR);
 
-      const docTextBefore = doc.getText();
+    assert.ok(await vscode.commands.executeCommand("quarto.test_isInVisualEditor"));
+  });
+  // Note: this test runs after the previous test, so `hello.qmd` has already been touched by the previous
+  //       test. That's okay for this test, but could cause issues if you expect a qmd to look how it
+  //       does in `/examples`.
+  test("Roundtrip doesn't change hello.qmd", async function () {
+    // don't run this in CI for now because we haven't figured out how to get the LSP to start
+    if (process.env['CI']) this.skip();
 
-      // switch to visual editor and back
-      await vscode.commands.executeCommand("quarto.test_setkVisualModeConfirmedTrue");
-      await wait(300);
-      await vscode.commands.executeCommand("quarto.editInVisualMode");
-      await wait(APPROX_TIME_TO_OPEN_VISUAL_EDITOR);
-      await vscode.commands.executeCommand("quarto.editInSourceMode");
-      await wait(300);
+    const doc = await vscode.workspace.openTextDocument(exampleWorkspaceOutPath("hello.qmd"));
+    const editor = await vscode.window.showTextDocument(doc);
 
-      const docTextAfter = doc.getText();
-      assert.ok(docTextBefore === docTextAfter);
-    });
-  }
+    const docTextBefore = doc.getText();
+
+    // switch to visual editor and back
+    await vscode.commands.executeCommand("quarto.test_setkVisualModeConfirmedTrue");
+    await wait(300);
+    await vscode.commands.executeCommand("quarto.editInVisualMode");
+    await wait(APPROX_TIME_TO_OPEN_VISUAL_EDITOR);
+    await vscode.commands.executeCommand("quarto.editInSourceMode");
+    await wait(300);
+
+    const docTextAfter = doc.getText();
+    assert.ok(docTextBefore === docTextAfter);
+  });
 });

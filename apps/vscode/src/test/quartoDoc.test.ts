b5cf796a5e11c4b47c8c7bf9abd5dc427773f334
elliot <elliot.evans@posit.co>
2025-07-25 15:51:07 -0400

    Add start of roundtrip-failures

--- apps/vscode/src/test/quartoDoc.test.ts
+++ apps/vscode/src/test/quartoDoc.test.ts
@@ -13,6 +13,7 @@ suite("Quarto basics", function () {
     await vscode.workspace.fs.delete(examplesOutUri(), { recursive: true });
     await vscode.workspace.fs.copy(vscode.Uri.file(WORKSPACE_PATH), examplesOutUri());
   });
+
   test("Can open a Quarto document", async function () {
     const doc = await vscode.workspace.openTextDocument(examplesOutUri("hello.qmd"));
     const editor = await vscode.window.showTextDocument(doc);
@@ -20,6 +21,7 @@ suite("Quarto basics", function () {
     assert.strictEqual(editor?.document.languageId, "quarto");
     assert.strictEqual(isQuartoDoc(editor?.document), true);
   });
+
   // Note: this test runs after the previous test, so `hello.qmd` can already be touched by the previous
   //       test. That's okay for this test, but could cause issues if you expect a qmd to look how it
   //       does in `/examples`.
@@ -27,17 +29,37 @@ suite("Quarto basics", function () {
     const doc = await vscode.workspace.openTextDocument(examplesOutUri("hello.qmd"));
     const editor = await vscode.window.showTextDocument(doc);
 
-    const docTextBefore = doc.getText();
+    const { before, after } = await roundtrip(doc);
 
-    // switch to visual editor and back
-    await vscode.commands.executeCommand("quarto.test_setkVisualModeConfirmedTrue");
-    await wait(300);
-    await vscode.commands.executeCommand("quarto.editInVisualMode");
-    await wait(APPROX_TIME_TO_OPEN_VISUAL_EDITOR);
-    await vscode.commands.executeCommand("quarto.editInSourceMode");
-    await wait(300);
+    assert.equal(before, after);
+  });
 
-    const docTextAfter = doc.getText();
-    assert.equal(docTextBefore, docTextAfter);
+  test("Roundtrip does change roundtrip-failures.qmd", async function () {
+    // We want this test to fail locally so that we can reference the
+    // before/affter diff that Mocha logs, but we dont wan't CI to fail.
+    if (process.env['CI']) this.skip();
+
+    const doc = await vscode.workspace.openTextDocument(examplesOutUri("roundtrip-failures.qmd"));
+    const editor = await vscode.window.showTextDocument(doc);
+
+    const { before, after } = await roundtrip(doc);
+
+    assert.equal(before, after);
   });
 });
+
+async function roundtrip(doc: vscode.TextDocument) {
+  const before = doc.getText();
+
+  // switch to visual editor and back
+  await vscode.commands.executeCommand("quarto.test_setkVisualModeConfirmedTrue");
+  await wait(300);
+  await vscode.commands.executeCommand("quarto.editInVisualMode");
+  await wait(APPROX_TIME_TO_OPEN_VISUAL_EDITOR);
+  await vscode.commands.executeCommand("quarto.editInSourceMode");
+  await wait(300);
+
+  const after = doc.getText();
+
+  return { before, after };
+}

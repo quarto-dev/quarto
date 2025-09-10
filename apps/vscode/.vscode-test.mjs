11e0a9f0e8a1bd0f0b0dab2b69586f1dc96dc3b4
Kevin Ushey <kevinushey@gmail.com>
2025-09-10 15:35:55 -0700

    remove debugging code

--- apps/vscode/.vscode-test.mjs
+++ apps/vscode/.vscode-test.mjs
@@ -1,20 +1,4 @@
 import { defineConfig } from '@vscode/test-cli';
-import { execSync } from 'child_process';
-
-const quartoPath = execSync('which quarto', {
-  encoding: 'utf-8'
-}).trim();
-
-const quartoVersion = execSync(`${quartoPath} --version`, {
-  encoding: 'utf-8',
-}).trim();
-
-console.info(`
-========================================
-Quarto Path:     ${quartoPath}
-Quarto Version:  ${quartoVersion}
-========================================
-`);
 
 export default defineConfig([
   {

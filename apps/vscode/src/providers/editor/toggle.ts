b15aa3818da7afe02f95682a4b0983207fc865a3
Julia Silge <julia.silge@gmail.com>
2025-10-01 10:41:34 -0600

    Force a user save on untitled documents, before switch to visual mode (#831)
    * Force a user save on untitled documents, before switch to visual mode
    
    * Update CHANGELOG
    
    * Fix for opening unsaved file in VE in VSCode
    
    * Make linter happy
    
    ---------
    
    Co-authored-by: elliot <elliot.evans@posit.co>

--- apps/vscode/src/providers/editor/toggle.ts
+++ apps/vscode/src/providers/editor/toggle.ts
@@ -13,12 +13,11 @@
  *
  */
 
-import { commands, window, workspace, TextDocument, ViewColumn } from "vscode";
+import { Uri, commands, window, workspace, TextDocument, ViewColumn } from "vscode";
 import * as quarto from "quarto-core";
 import { Command } from "../../core/command";
 import { isQuartoDoc, kQuartoLanguageId } from "../../core/doc";
 import { VisualEditorProvider } from "./editor";
-import { Uri } from "vscode";
 import { hasHooks } from "../../host/hooks";
 import { toggleEditMode, toggleRenderOnSaveOverride } from "../context-keys";
 
@@ -123,20 +122,25 @@ export async function reopenEditorInVisualMode(
   if (hasHooks()) {
     // note pending switch to visual
     VisualEditorProvider.recordPendingSwitchToVisual(document);
+    // if document is untitled, force user to save first
+    if (document.isUntitled) {
+      await commands.executeCommand("workbench.action.files.save");
+    }
+    // reopen in visual mode
     commands.executeCommand('positron.reopenWith', document.uri, 'quarto.visualEditor');
   } else {
-    // save then close
+    workspace.onDidSaveTextDocument(async (doc: TextDocument) => {
+      // open in visual mode
+      VisualEditorProvider.recordPendingSwitchToVisual(doc);
+      await commands.executeCommand('workbench.action.closeActiveEditor');
+      await commands.executeCommand("vscode.openWith",
+        doc.uri,
+        VisualEditorProvider.viewType,
+        { viewColumn }
+      );
+    });
+    // save, which will trigger `onDidSaveTextDocument`
     await commands.executeCommand("workbench.action.files.save");
-    await commands.executeCommand('workbench.action.closeActiveEditor');
-    VisualEditorProvider.recordPendingSwitchToVisual(document);
-    // open in visual mode
-    await commands.executeCommand("vscode.openWith",
-      document.uri,
-      VisualEditorProvider.viewType,
-      {
-        viewColumn
-      }
-    );
   }
 }
 

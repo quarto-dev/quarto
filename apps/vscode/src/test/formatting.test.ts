43078114a17c666cf6fa623977757825b9142aad
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2025-10-06 00:12:08 +0200

    test: add debugging code

--- apps/vscode/src/test/formatting.test.ts
+++ apps/vscode/src/test/formatting.test.ts
@@ -2,6 +2,9 @@ import * as vscode from "vscode";
 import * as assert from "assert";
 import * as path from "path";
 import { openAndShowTextDocument, wait, WORKSPACE_PATH } from "./test-utils";
+import { MarkdownEngine } from "../markdown/engine"; // DEBUGGING
+import { embeddedDocumentFormattingProvider } from "../providers/format"; // DEBUGGING
+import { isQuartoDoc } from "../core/doc"; // DEBUGGING
 
 suite("Code Block Formatting", function () {
   test("Format Python code block", async function () {
@@ -16,8 +19,29 @@ suite("Code Block Formatting", function () {
     const position = new vscode.Position(7, 0); // Line with "1+1"
     editor.selection = new vscode.Selection(position, position);
     await wait(1000);
-    await vscode.commands.executeCommand("quarto.formatCell");
-    // await vscode.commands.executeCommand("vscode.executeFormatDocumentProvider", doc.uri);
+
+    // DEBUGGING
+    // Use embeddedDocumentFormattingProvider directly
+    assert.strictEqual(isQuartoDoc(editor?.document), true);
+    const engine = new MarkdownEngine();
+    const provider = embeddedDocumentFormattingProvider(engine);
+    const next = async () => null;
+    const edits = await provider(
+      doc,
+      { tabSize: 4, insertSpaces: true },
+      new vscode.CancellationTokenSource().token,
+      next
+    );
+    assert.notStrictEqual(edits, null, "Provider should return edits");
+    assert.notStrictEqual(edits, undefined, "Provider should return edits");
+    assert.ok(Array.isArray(edits), "Edits should be an array");
+    assert.ok(edits.length > 0, "Provider should return at least one edit");
+    const editApplied = await editor.edit((editBuilder) => {
+      edits.forEach((edit) => {
+        editBuilder.replace(edit.range, edit.newText);
+      });
+    });
+    assert.strictEqual(editApplied, true, "Edits should be successfully applied");
 
     await wait(500);
 

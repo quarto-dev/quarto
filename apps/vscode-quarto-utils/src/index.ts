efd8f8c2344e79a33b2488d8333ce29b359f754d
Julia Silge <julia.silge@gmail.com>
2025-11-05 08:27:12 -0700

    Extract `utils` into separate package and bundle it when building

--- apps/vscode-quarto-utils/src/index.ts
+++ apps/vscode-quarto-utils/src/index.ts
@@ -1,5 +1,5 @@
 /*
- * r-utils.ts
+ * index.ts
  *
  * Copyright (C) 2025 by Posit Software, PBC
  *
@@ -13,17 +13,4 @@
  *
  */
 
-import * as vscode from 'vscode';
-import { isRPackage as isRPackageImpl } from "vscode-quarto-utils";
-
-// Version that selects workspace folder
-export async function isRPackage(): Promise<boolean> {
-  if (vscode.workspace.workspaceFolders === undefined) {
-    return false;
-  }
-
-  // Pick first workspace
-  const workspaceFolder = vscode.workspace.workspaceFolders[0];
-
-  return isRPackageImpl(workspaceFolder.uri);
-}
+export * from './r-utils';

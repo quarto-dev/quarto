c01a0cbccf018667e4b6833562acad2bf06babd8
Davis Vaughan <davis@posit.co>
2026-02-12 15:32:31 -0500

    Copyright year

--- apps/vscode/src/vdoc/vdoc.ts
+++ apps/vscode/src/vdoc/vdoc.ts
@@ -1,7 +1,7 @@
 /*
  * vdoc.ts
  *
- * Copyright (C) 2022-2025 by Posit Software, PBC
+ * Copyright (C) 2022-2026 by Posit Software, PBC
  *
  * Unless you have received this program directly from Posit Software pursuant
  * to the terms of a commercial license agreement with Posit Software, then

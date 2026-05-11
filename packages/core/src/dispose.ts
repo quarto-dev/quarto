8d8847eb7c8b03d093ed947748cebedb680f3ef5
seem <mwlorgat@gmail.com>
2026-05-11 17:39:17 +0200

    add disposablestore.clear and format

--- packages/core/src/dispose.ts
+++ packages/core/src/dispose.ts
@@ -15,81 +15,95 @@
  */
 
 export interface IDisposable {
-	dispose(): void;
+  dispose(): void;
 }
 
 export class MultiDisposeError extends Error {
-	constructor(
-		public readonly errors: unknown[]
-	) {
-		super(`Encountered errors while disposing of store. Errors: [${errors.join(', ')}]`);
-	}
+  constructor(
+    public readonly errors: unknown[]
+  ) {
+    super(`Encountered errors while disposing of store. Errors: [${errors.join(', ')}]`);
+  }
 }
 
 export function disposeAll(disposables: Iterable<IDisposable>) {
-	const errors: unknown[] = [];
+  const errors: unknown[] = [];
 
-	for (const disposable of disposables) {
-		try {
-			disposable.dispose();
-		} catch (e) {
-			errors.push(e);
-		}
-	}
+  for (const disposable of disposables) {
+    try {
+      disposable.dispose();
+    } catch (e) {
+      errors.push(e);
+    }
+  }
 
-	if (errors.length === 1) {
-		throw errors[0];
-	} else if (errors.length > 1) {
-		throw new MultiDisposeError(errors);
-	}
+  if (errors.length === 1) {
+    throw errors[0];
+  } else if (errors.length > 1) {
+    throw new MultiDisposeError(errors);
+  }
 }
 
 export interface IDisposable {
-	dispose(): void;
+  dispose(): void;
 }
 
 export abstract class Disposable {
-	#isDisposed = false;
+  #isDisposed = false;
 
-	protected _disposables: IDisposable[] = [];
+  protected _disposables: IDisposable[] = [];
 
-	public dispose()  {
-		if (this.#isDisposed) {
-			return;
-		}
-		this.#isDisposed = true;
-		disposeAll(this._disposables);
-	}
+  public dispose() {
+    if (this.#isDisposed) {
+      return;
+    }
+    this.#isDisposed = true;
+    disposeAll(this._disposables);
+  }
 
-	protected _register<T extends IDisposable>(value: T): T {
-		if (this.#isDisposed) {
-			value.dispose();
-		} else {
-			this._disposables.push(value);
-		}
-		return value;
-	}
+  protected _register<T extends IDisposable>(value: T): T {
+    if (this.#isDisposed) {
+      value.dispose();
+    } else {
+      this._disposables.push(value);
+    }
+    return value;
+  }
 
-	protected get isDisposed() {
-		return this.#isDisposed;
-	}
+  protected get isDisposed() {
+    return this.#isDisposed;
+  }
 }
 
 export class DisposableStore extends Disposable {
-	readonly #items = new Set<IDisposable>();
+  readonly #items = new Set<IDisposable>();
 
-	public override dispose() {
-		super.dispose();
-		disposeAll(this.#items);
-		this.#items.clear();
-	}
+  public override dispose() {
+    super.dispose();
+    this.clear();
+  }
 
-	public add<T extends IDisposable>(item: T): T {
-		if (this.isDisposed) {
-			console.warn('Adding to disposed store. Item will be leaked');
-		}
+  public add<T extends IDisposable>(item: T): T {
+    if (this.isDisposed) {
+      console.warn('Adding to disposed store. Item will be leaked');
+    }
 
-		this.#items.add(item);
-		return item;
-	}
+    this.#items.add(item);
+    return item;
+  }
+
+  /**
+   * Dispose of all registered disposables but do not mark this object as disposed.
+   */
+  public clear(): void {
+    if (this.#items.size === 0) {
+      return;
+    }
+
+    try {
+      disposeAll(this.#items);
+    } finally {
+      this.#items.clear();
+    }
+  }
 }
